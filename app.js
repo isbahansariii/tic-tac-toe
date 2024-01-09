0abb8842e2dc5bd7a15cfffd771f70867d154a35
isbahansariii <ansariisbah427@gmail.com>
2024-01-09 21:06:24 +0500

    app.js

--- app.js
+++ app.js
@@ -17,28 +17,67 @@ const winningPatterns = [
 ];
 let turn = true;
 let i = 0;
-let player1, player2;
+let player1, player2; //for storing name of player1 and player2
 let start = true;
 let boolWinner = true;
+let resetGameBtnClick = 0; // no of time reset game button is clicked
+let playOne = 0,
+  playTwo = 0; // score purpose
+
+const winnerBtnColor = (p1, p2, p3) => {
+  // here p1, p2, p3 is buttons
+  p1.style.backgroundColor = "#820300";
+  p2.style.backgroundColor = "#820300";
+  p3.style.backgroundColor = "#820300";
+};
+
+const resetBtnColor = () => {
+  for (let button of buttons) {
+    button.style.backgroundColor = "#b80000";
+  }
+};
 
 let getName = () => {
   player1 = prompt("Enter name of player#1: ");
   player2 = prompt("Enter name of player#2: ");
 };
+
 let newGameBtn = () => {
+  (playOne = 0), (playTwo = 0);   //score will set to 0
+  resetGameBtnClick = 0;          // it will not show the score of previous game
+  win.classList.remove("class", "winScore");    //score style will remove
   i = 0;
   turn = true;
-  enableBtns();
   getName();
+  enableBtns();
+  resetBtnColor();
   container.classList.add("class", "hide");
 };
+
+const score = (winner) => {
+  //score take O or X
+  if (winner === "O") {
+    playOne++;
+  } else {
+    playTwo++;
+  }
+};
+
+const showScore = () => {
+  win.classList.add("class", "winScore"); //score styling will add
+  win.innerText = `${player1}: ${playOne}\n${player2}: ${playTwo}`;
+};
+
 let resetBtn = () => {
   i = 0;
   turn = true;
-  enableBtns();
+  resetGameBtnClick=1;
   boolWinner = true;
   container.classList.add("class", "hide");
+  enableBtns();
+  resetBtnColor();
 };
+
 const disableBtns = () => {
   for (let btn of buttons) {
     btn.disabled = true;
@@ -53,13 +92,20 @@ const enableBtns = () => {
 
 const showWinner = (winner) => {
   disableBtns();
-  if (winner === "O")   win.innerText = `Congradulations, ${player1} won.`;
-  else                  win.innerText = `Congradulations, ${player2} won.`;
+  score(winner);
+  if (resetGameBtnClick > 0)
+    showScore(); //if one time click on resetGameBtn then save the previous score and show it
+  else {
+    if (winner === "O") {
+      win.innerText = `Congradulations, ${player1} won.`;
+    } else {
+      win.innerText = `Congradulations, ${player2} won.`;
+    }
+  }
   container.classList.remove("hide");
 };
 
 let Draw = () => {
-  console.log(boolWinner)
   if (i === 9 && boolWinner === true) {
     win.innerText = `Match Draw!`;
     container.classList.remove("hide");
@@ -75,10 +121,16 @@ const checkWinner = () => {
     if (p1 != "" && p2 != "" && p3 != "")
       if (p1 === p2 && p2 === p3) {
         showWinner(p1);
+        winnerBtnColor(
+          buttons[pattern[0]],
+          buttons[pattern[1]],
+          buttons[pattern[2]]
+        );
         boolWinner = false;
       }
   }
 };
+
 
 buttons.forEach((button) => {
   if (start) {
